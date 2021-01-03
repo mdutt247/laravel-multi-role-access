fc7a6d555f9d9039e236b2e2b5736d5c3b27717d
mdutt247 <59934065+mdutt247@users.noreply.github.com>
2021-01-04 00:02:29 +0530

    fied broken menu

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,16 @@
-<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-    Admin role page
-</h2>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Admin') }}
+        </h2>
+    </x-slot>
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div class="mb-4">
+                    Admin role page
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
